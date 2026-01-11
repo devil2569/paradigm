e10a44c691c57f8d91a958bdfe447257c636d881
devil2569 <ivanpominov29@gmail.com>
2026-01-10 18:20:26 -0800

    Create regions.h

---
+++ hv/regions.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <ntddk.h>
+#include <intrin.h>
+
+#include "ia32.hpp"
+#include "commons.hpp"
+#include "util.h"
+
+namespace paradigm
+{
+	class regs final {
+	public:
+		auto allocate_vmx_reg(vcpu* vcpu) -> bool;
+
+		auto allocate_vmcs_reg(vcpu* vcpu) -> bool;
+
+		auto allocate_regions(vcpu* vcpu) -> bool;
+	};
+}
+
+extern paradigm::regs* regions;
