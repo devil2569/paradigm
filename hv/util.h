0cb18595fab50ceafa605f19c99e029144164d7f
devil2569 <ivanpominov29@gmail.com>
2026-01-12 11:59:49 -0800

    Add files via upload

--- hv/util.h
+++ hv/util.h
@@ -2,6 +2,8 @@
 #include <ntddk.h>
 #include <intrin.h>
 #include <cstdint>
+#include "ia32.hpp"
+#include "commons.hpp"
 
 namespace paradigm
 {
@@ -19,6 +21,51 @@ namespace paradigm
 			physical_addr.QuadPart = phys;
 			return reinterpret_cast<uint64_t>(MmGetVirtualForPhysical(physical_addr));
 		}
+
+		inline auto segment_base(
+			segment_descriptor_register_64 const& gdtr,
+			segment_selector const selector) -> uint64_t {
+
+			if (selector.index == 0)
+				return 0;
+
+			auto const descriptor = reinterpret_cast<segment_descriptor_64*>(
+				gdtr.base_address + static_cast<uint64_t>(selector.index) * 8);
+
+			auto base_address =
+				(uint64_t)descriptor->base_address_low |
+				((uint64_t)descriptor->base_address_middle << 16) |
+				((uint64_t)descriptor->base_address_high << 24);
+
+			if (descriptor->descriptor_type == SEGMENT_DESCRIPTOR_TYPE_SYSTEM)
+				base_address |= (uint64_t)descriptor->base_address_upper << 32;
+
+			return base_address;
+		}
+
+		inline unsigned __int32 access_right(unsigned __int16 segment_selector)
+		{
+			__segment_selector_t selector;
+			__segment_access_rights_t vmx_access_rights;
+
+			selector.flags = segment_selector;
+
+			if (selector.table == 0
+				&& selector.index == 0)
+			{
+				vmx_access_rights.flags = 0;
+				vmx_access_rights.unusable = TRUE;
+				return vmx_access_rights.flags;
+			}
+
+			vmx_access_rights.flags = (__load_ar(segment_selector) >> 8);
+			vmx_access_rights.unusable = 0;
+			vmx_access_rights.reserved0 = 0;
+			vmx_access_rights.reserved1 = 0;
+
+			return vmx_access_rights.flags;
+		}
+
 	};
 }
 
