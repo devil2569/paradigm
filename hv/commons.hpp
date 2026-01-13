9d79bd718c360da3e6c89fc9f500316caa0bf5b3
devil2569 <ivanpominov29@gmail.com>
2026-01-12 17:15:36 -0800

    Update commons.hpp

--- hv/commons.hpp
+++ hv/commons.hpp
@@ -21,7 +21,7 @@ typedef struct hypervisor
     vcpu* vcpus;
 } hypervisor;
 
-typedef union __segment_selector_t
+typedef union hv_segment_selector
 {
     struct
     {
@@ -30,9 +30,9 @@ typedef union __segment_selector_t
         unsigned __int16 index : 13;
     };
     unsigned __int16 flags;
-} segment_selector;
+} hv_segment_selector;
 
-typedef union __segment_access_rights_t
+typedef union hv_segment_access_rights
 {
     struct
     {
@@ -49,7 +49,47 @@ typedef union __segment_access_rights_t
         unsigned __int32 reserved1 : 15;
     };
     unsigned __int32 flags;
-} segment_access_rights;
+} hv_segment_access_rights;
+
+
+typedef struct _guest_registers
+{
+    __m128 xmm0;
+    __m128 xmm1;
+    __m128 xmm2;
+    __m128 xmm3;
+    __m128 xmm4;
+    __m128 xmm5;
+    __m128 xmm6;
+    __m128 xmm7;
+    __m128 xmm8;
+    __m128 xmm9;
+    __m128 xmm10;
+    __m128 xmm11;
+    __m128 xmm12;
+    __m128 xmm13;
+    __m128 xmm14;
+    __m128 xmm15;
+
+    uint64_t padding_8b;
+
+    uint64_t r15;
+    uint64_t r14;
+    uint64_t r13;
+    uint64_t r12;
+    uint64_t r11;
+    uint64_t r10;
+    uint64_t r9;
+    uint64_t r8;
+    uint64_t rbp;
+    uint64_t rdi;
+    uint64_t rsi;
+    uint64_t rdx;
+    uint64_t rcx;
+    uint64_t rbx;
+    uint64_t rax;
+} guest_registers, * pguest_registers;
+
 
 extern "C" unsigned __int32 __load_ar(unsigned __int16 segment_selector);
 extern "C" segment_selector get_cs(void);
@@ -63,5 +103,7 @@ extern "C" segment_selector get_tr(void);
 
 extern "C" void _sgdt(segment_descriptor_register_64* gdtr);
 
+extern "C" void asm_vmexit_stub(void);
+extern "C" bool vmlaunch_asm(void);
 
 extern hypervisor* hv;
